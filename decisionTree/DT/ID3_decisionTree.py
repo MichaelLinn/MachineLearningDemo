680d2e3a90f501225f0f8e2fd9ddd18e60d48041
MichaelLinnn <linjiecong@vip.qq.com>
2016-08-16 14:59:22 +0800

    DT update

--- decisionTree/DT/ID3_decisionTree.py
+++ decisionTree/DT/ID3_decisionTree.py
@@ -2,7 +2,6 @@
 
 """
 Created on Thu Jul 28 12:36:46 2016
-
 @author: michael
 """
 
@@ -15,11 +14,13 @@ class ID3_decisionTree:
     def __init__(self,dataSet):
         self.dataSet = dataSet
 
-
-    def calcShannonEnt(self):
-        numEntries = len(self.dataSet)
+#基于特征值A，把D分成D_1,D_2,...,D_i,...,D_n, 此处计算 H(D_i|A = a_i)
+# empirical entropy = sum_i( p_i * H(D|A = a_i) )
+#                   = sum_i ( |D_i|/|D| * sum_k( - (|D_ik| / |D_i|) log (|D_ik| / |D_i|) ))
+    def calcShannonEnt(self,dataSet):
+        numEntries = len(dataSet)
         labelCounts = {}
-        for featVec in self.dataSet:
+        for featVec in dataSet:
             currentLabel = featVec[-1]
             labelCounts[currentLabel] = labelCounts.get(currentLabel,0) + 1
         shannonEnt = 0.0
@@ -28,7 +29,7 @@ class ID3_decisionTree:
             shannonEnt += -(prob*log(prob,2))
         return shannonEnt
 
-#在数据集D中去掉特征值A，方便计算基于属性A_i划分后的数据集D_n的熵
+#在数据集D中去掉特征值A_i，方便计算基于属性A_i划分后的数据集D_n的熵
     def splitDataSet(self,dataSet,axis,value):
         retDataSet = []
         for featVec in dataSet:
@@ -37,6 +38,25 @@ class ID3_decisionTree:
                 reduceFeatVec.extend(featVec[axis+1:])
                 retDataSet.append(reduceFeatVec)
         return retDataSet
+
+    def chooseBestFeatureToSplit(self,dataSet):
+#       特征值数量
+        numFeatures = len(dataSet[0] - 1)
+        baseEntropy = self.calcShannonEnt(self.dataSet)
+        baseInfoGain = 0.0
+        beastFeature = -1
+        for i in numFeatures:
+            featList = [example[i] for example in dataSet]
+        # 用集合的形式存储属性A的所有取值，重复的值只保存一次
+            uniqueVals = set(featList)
+
+
+
+
+
+
+
+
 
 
 def main():
