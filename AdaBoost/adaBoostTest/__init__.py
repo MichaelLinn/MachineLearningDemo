f29e38109a7f9cfa51a6ca430072958a08c44a2d
MichaelLinn <linjiecong@vip.qq.com>
2016-09-09 15:00:55 +0800

    AdaBoost

---
+++ AdaBoost/adaBoostTest/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding:utf-8 -*- 
+"""
+Created on '2016/9/9' '10:32'
+
+@author: 'michael"  
+"""
+   
---
+++ AdaBoost/adaBoostTest/adaBoost.py
@@ -0,0 +1,12 @@
+# -*- coding:utf-8 -*- 
+"""
+Created on '2016/9/9' '10:32'
+
+@author: 'michael"  
+"""
+
+import numpy as np
+
+class adaBoost:
+
+
